771df81bfa76f08ba6b49d824d2f3e9f6650c7d9
danison <danison.santos00@gmail.com>
2023-09-04 11:07:46 -0300

    Security Configuration

---
+++ src/main/java/com/dan/jwt/infra/SecurityConfigurations.java
@@ -0,0 +1,26 @@
+package com.dan.jwt.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.dan.jwt.domain.Roles;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http.csrf(csrf-> csrf.disable())
+               .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").hasRole(Roles.ADMIN.getRole()))
+               .build();
+        
+    }
+    
+}
