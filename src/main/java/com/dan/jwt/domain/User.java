2af5060b2c10ad7758411252713c357db025a5f9
danison <danison.santos00@gmail.com>
2023-09-04 11:07:28 -0300

    update user

--- src/main/java/com/dan/jwt/domain/User.java
+++ src/main/java/com/dan/jwt/domain/User.java
@@ -34,7 +34,7 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
        
-        return List.of(new SimpleGrantedAuthority(roles.getRole()));
+        return List.of(new SimpleGrantedAuthority(roles.USER.getRole()));
 
     }
 
